0180ca3c6a704ee9ad0dd80b9b5aaae0c21ad8cd
Daniel Banks <danb97@protonmail.com>
2022-09-08 12:21:54 +0100

    now displays movie details when list item clicked

--- src/views/DetailsView/DetailsView.tsx
+++ src/views/DetailsView/DetailsView.tsx
@@ -3,10 +3,11 @@ import BackIcon from '@mui/icons-material/ArrowBack'
 import AddIcon from '@mui/icons-material/Add'
 
 import { useAppSelector, useAppDispatch } from '../../redux/hooks'
-import { setMovie } from '../../redux/slices/moviesSlice'
+import { selectMovieDetails } from '../../redux/slices/moviesSlice'
 import { useNavigate } from 'react-router-dom'
 
 const DetailsView = () => {
+    const details = useAppSelector(selectMovieDetails)
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
 
@@ -27,17 +28,17 @@ const DetailsView = () => {
                 <CardMedia
                     component="img"
                     height="480"
-                    image="https://assets-prd.ignimgs.com/2022/08/16/better-call-saul-hero-1660667483204.jpg?width=1920"
-                    alt="bcs"
+                    image={details.poster}
+                    alt={`${details.title} poster`}
                 />
                 <CardContent sx={{ borderTopLeftRadius: '8px', borderTopRightRadius: '8px' }}>
                     <Box component="div" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                         <Box component="div" sx={{ display: 'flex', alignItems: 'end' }}>
                             <Typography sx={{ mr: '8px' }} variant="h4" component="div">
-                                Movie Title
+                                {details.title} 
                             </Typography>
                             <Typography sx={{ color: 'gray', lineHeight: 1.7 }} variant="h6" component="div">
-                                2017
+                                {details.year}
                             </Typography>
                         </Box>
                         <Button onClick={() => console.log('add')} variant="contained" size="small" startIcon={<AddIcon />} sx={{ backgroundColor: '#63e6be' }}>Add</Button>
