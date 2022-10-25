7f4b95284a5ec117f79f72f95dfd530439760b7c
Daniel Banks <danb97@protonmail.com>
2022-10-25 16:41:08 +0100

    add lists through side menu

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,30 +1,62 @@
-import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { useState, useEffect } from 'react'
+import { Box, Button, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField, Input } from "@mui/material"
 import SyncIcon from '@mui/icons-material/Sync'
 import TaskAltIcon from '@mui/icons-material/TaskAlt'
 import ListIcon from '@mui/icons-material/List'
+import AddIcon from '@mui/icons-material/Add'
+import { createNewList, getAllLists, List as ListType } from '../../firebase/queries'
 
 interface SideMenuProps {
     isOpen: boolean,
     toggle: (event: React.KeyboardEvent | React.MouseEvent) => void
 }
 
-const listElements: Array<string> = ['Seen', 'In Progress', 'Plan to Watch']
-
 const SideMenu = ({ isOpen, toggle }: SideMenuProps) => {
+    const [refreshing, setRefreshing] = useState(true)
+    const [newListLabel, setNewListLabel] = useState('');
+    const [listElements, setListElements] = useState<ListType[]>([])
+
+    useEffect(() => {
+        if (refreshing === true) (refreshLists)()
+    }, [refreshing])
+
+    const changeNewListLabel = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void => {
+        setNewListLabel(event.target.value)
+    }
+
+    const addNewListHandler = (): void => {
+        createNewList(newListLabel)
+        setNewListLabel('')
+        setRefreshing(true)
+    }
+
+    const refreshLists = async (): Promise<void> => {
+        const data = await getAllLists()
+        setListElements(data)
+        setRefreshing(false)
+    }
+
     return (
         <Drawer anchor="left" open={isOpen} onClose={toggle}>
             <Box sx={{ width: 250, height: '100%', backgroundColor: '#343a40', color: '#FFF' }}>
                 <List>
-                    {listElements.map((label, index) => (
-                            <ListItem key={label} disablePadding>
+                    {listElements.map((list, _) => (
+                            <ListItem key={list.label} disablePadding>
                                 <ListItemButton>
                                     <ListItemIcon>
-                                        {index === 0 ? <TaskAltIcon sx={{ color: '#FFF' }} /> : index === 1 ? <SyncIcon sx={{ color: '#FFF' }} /> : <ListIcon sx={{ color: '#FFF' }} />}
+                                        {/*index === 0 ? <TaskAltIcon sx={{ color: '#FFF' }} /> : index === 1 ? <SyncIcon sx={{ color: '#FFF' }} /> : <ListIcon sx={{ color: '#FFF' }} />*/}
+                                        <ListIcon sx={{ color: '#FFF' }} />
                                     </ListItemIcon>
-                                    <ListItemText primary={label} />
+                                    <ListItemText primary={list.label} />
                                 </ListItemButton>
                             </ListItem>
                     ))}
+                    <ListItem>
+                        <TextField onChange={changeNewListLabel} value={newListLabel} sx={{ label: { display: 'none' }, input: { backgroundColor: '#fff' } }} label="New List Title" size="small" variant="filled" hiddenLabel InputProps={{ disableUnderline: true }} />
+                        <Button onClick={addNewListHandler} sx={{ height: 40, width: 42, minWidth: 0,  backgroundColor: '#099268' }} variant="contained">
+                            <AddIcon sx={{ color: '#FFF' }} />
+                        </Button>
+                    </ListItem>
                 </List>
             </Box>
         </Drawer>
--- src/firebase/queries.ts
+++ src/firebase/queries.ts
@@ -1,4 +1,35 @@
 import { app } from './initialise'
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
+import { getFirestore, collection, getDocs, doc, setDoc} from 'firebase/firestore/lite'
+
+type Movie = {
+    title: string
+}
+
+export type List = {
+    label: string,
+    movies: Array<Movie>
+}
 
 const db = getFirestore(app)
+
+export const getAllLists = async (): Promise<List[]> => {
+    let lists: List[] = []
+    const querySnapshot = await getDocs(collection(db, "lists"))
+    querySnapshot.forEach((doc) => {
+        const docData: List = {
+            label: doc.id,
+            movies: doc.data().movies
+        }
+        lists.push(docData)
+    })
+
+    return lists
+} 
+const getList = () => {}
+export const createNewList = async (listLabel: string): Promise<void> => {
+    await setDoc(doc(db, "lists", listLabel), {
+        movies: []
+    });
+}
+const addToList = () => {}
+const deleteList = () => {}
